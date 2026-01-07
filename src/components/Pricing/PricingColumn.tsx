d55f937e1b241de5cd54dbd9768ec46023e49bc7
Darío57923_ <120041144+dario57923@users.noreply.github.com>
2026-01-07 17:05:05 +0100

    Change button styles based on highlight state

--- src/components/Pricing/PricingColumn.tsx
+++ src/components/Pricing/PricingColumn.tsx
@@ -21,7 +21,7 @@ const PricingColumn: React.FC<Props> = ({ tier, highlight }: Props) => {
                     </span>
                     {typeof price === 'number' && <span className="text-lg font-normal text-gray-600">/mo</span>}
                 </p>
-                <button className={clsx("w-full py-3 px-4 rounded-full transition-colors", { "bg-primary hover:bg-primary-accent": highlight, "bg-hero-background hover:bg-gray-200": !highlight })}>
+                <button className={clsx("w-full py-3 px-4 rounded-full transition-colors", { "bg-black text-white hover:bg-gray-800": highlight, "bg-hero-background hover:bg-gray-200": !highlight })}>
                     Comenzar ahora
                 </button>
             </div>
